975cedaff6d26b2a224f5ea0ee4b5696145df727
Ren Pang <ain-soph@live.com>
2021-10-09 12:18:36 -0400

    update theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,8 +25,7 @@ gh_page_url = f'https://{github_user}.github.io/{github_repo}/'
 
 html_theme_options = {
     'canonical_url': gh_page_url,
-    'collapse_navigation': False,
-    'display_version': True,
+    'github_url': github_url,
 
     'doc_items': {
         'AlpsPlot': '/alpsplot',
@@ -34,7 +33,6 @@ html_theme_options = {
         'trojanzoo_sphinx_theme': '/trojanzoo_sphinx_theme',
         'base': 'https://github.com/ain-soph/base',
     },
-    'github_url': github_url,
 
     'logo': 'images/logo/trojanzoo-logo.svg',
     'logo_dark': 'images/logo/trojanzoo-logo-dark.svg',
